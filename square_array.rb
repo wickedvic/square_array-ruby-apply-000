ffdb9aacde2e3f25bf147ee1384e5a702c5d8571
Vivek Sanghvi <viksipart@gmail.com>
2020-09-04 23:42:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,9 @@
-#def square_array(array)
-#  new_arr = []
- # array.each do |num|
- #   new_arr << (num * num)
-  #  end
-    
-  #  return new_arr
-#end
-
 def square_array(array)
-  array.collect do |num|
-    num**2
-  
+  new_arr = []
+ array.each do |num|
+    new_arr << (num * num)
+    end
+    
+   return new_arr
 end
-  
-end
+
